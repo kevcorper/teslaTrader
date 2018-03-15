300b18a394a98dda4cf299b3e51b3b6acac896c8
Kevin Perkins <kevin.p@ITs-MBP-3.home>
2018-03-14 20:38:38 -0400

    initialize project

---
+++ Tesla Trader/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tesla Trader
+//
+//  Created by Kevin Perkins on 3/14/18.
+//  Copyright © 2018 Kevin Perkins. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
