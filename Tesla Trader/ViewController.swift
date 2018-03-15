dbb4b9ec03c65c0b219f008a0b3ad129e16daad7
Kevin Perkins <kevin.p@ITs-MBP-3.home>
2018-03-14 21:40:32 -0400

    update main calculation page with stack view and outlet/action connections

--- Tesla Trader/ViewController.swift
+++ Tesla Trader/ViewController.swift
@@ -10,6 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var stackView: UIStackView!
+    @IBOutlet weak var model: UISegmentedControl!
+    @IBOutlet weak var upgrades: UISegmentedControl!
+    @IBOutlet weak var mileageLabel: UILabel!
+    @IBOutlet weak var mileage: UISlider!
+    @IBOutlet weak var condition: UISegmentedControl!
+    @IBOutlet weak var evaluation: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +28,9 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func calculateValue(_ sender: Any) {
+        
+    }
+    
 }
 
